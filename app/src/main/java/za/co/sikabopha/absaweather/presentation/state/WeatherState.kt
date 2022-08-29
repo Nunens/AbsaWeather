c083621f0777c13350df20b2009c4a229c15e3ce
S Kobue <siphokobue@gmail.com>
2022-08-29 14:13:22 +0200

    Created weather state class for managing state on the presentation layer

---
+++ app/src/main/java/za/co/sikabopha/absaweather/presentation/state/WeatherState.kt
@@ -0,0 +1,11 @@
+package za.co.sikabopha.absaweather.presentation.state
+
+import za.co.sikabopha.absaweather.domain.model.Weather
+
+data class WeatherState(
+    val weatherList: List<Weather> = emptyList(),
+    val error: String = "",
+    val selectedIndex: Int = 0,
+    val isLoading: Boolean = false,
+    val firstLaunch: Boolean = true
+)
