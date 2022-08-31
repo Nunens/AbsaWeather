611aa05d7a3a9ca17f2b872833dcc22325dcc709
S Kobue <siphokobue@gmail.com>
2022-08-31 14:05:02 +0200

    Fixed URL

--- app/src/main/java/za/co/sikabopha/absaweather/data/remote/WeatherApi.kt
+++ app/src/main/java/za/co/sikabopha/absaweather/data/remote/WeatherApi.kt
@@ -6,14 +6,9 @@ import java.util.*
 
 interface WeatherApi {
     companion object {
-        const val BASE_URL = "https://api.tomorrow.io/v4/"
-        //timelines?location=-73.98529171943665,40.75872069597532&fields=
-        // temperature&timesteps=1h&units=metric&
-        // apikey=pI5Yg97fDKwgcrxGMjUn1JoP8MA4ALCS"
-        //TODO: below is a full curl script - produces json response
-        //curl --request GET --url 'https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=pI5Yg97fDKwgcrxGMjUn1JoP8MA4ALCS'
-    }
+        const val BASE_URL = "https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&fields=temperature&timesteps=1h&units=metric&apikey=pI5Yg97fDKwgcrxGMjUn1JoP8MA4ALCS"
+   }
 
-    @GET("timelines")
+    @GET
     suspend fun getWeather(): Data
 }
