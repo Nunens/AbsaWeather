df84516f684580a41c38e9df6c91534d05589b17
S Kobue <siphokobue@gmail.com>
2022-08-31 12:42:26 +0200

    Created view model

---
+++ app/src/main/java/za/co/sikabopha/absaweather/presentation/viewmodel/WeatherViewmodel.kt
@@ -0,0 +1,49 @@
+package za.co.sikabopha.absaweather.presentation.viewmodel
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import za.co.sikabopha.absaweather.domain.repository.WeatherRepository
+import za.co.sikabopha.absaweather.presentation.state.WeatherState
+import javax.inject.Inject
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.collect
+import za.co.sikabopha.absaweather.domain.resource.Resource
+
+@HiltViewModel
+class WeatherViewmodel @Inject constructor() : ViewModel() {
+    @Inject
+    lateinit var repository: WeatherRepository
+
+    private val _state = mutableStateOf(WeatherState())
+    val state: State<WeatherState> = _state
+
+    fun getWeather(){
+        viewModelScope.async{
+            repository.getWeather()
+                .collect { resp ->
+                    when(resp){
+                        is Resource.Loading -> {
+                            _state.value = state.value.copy(
+                                isLoading = resp.isLoading
+                            )
+                        }
+                        is Resource.Success -> {
+                            resp.data?.let {
+                                _state.value = state.value.copy(
+                                    weatherList = it
+                                )
+                            }
+                        }
+                        else -> {
+                            _state.value = state.value.copy(
+                                error = resp.message.toString()
+                            )
+                        }
+                    }
+                }
+        }
+    }
+}
